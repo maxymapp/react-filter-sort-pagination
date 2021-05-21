472748efa719baa14a0e70ffee0d3497d8b244ac
Maksym Kulikovskiy <maxymapp@gmail.com>
2021-05-21 14:42:09 -0700

    converted redundant extraReducers to reducers (actions)

--- src/store/index.js
+++ src/store/index.js
@@ -5,42 +5,6 @@ export const fetchAudiences = createAsyncThunk("audiences", async () => {
     return await client.get("/audiences");
 });
 
-const SORT_BY_ID = "SORT_BY_ID";
-const SORT_BY_NAME = "SORT_BY_NAME";
-const SORT_BY_SIZE_TOTAL = "SORT_BY_SIZE_TOTAL";
-
-const SEARCH_BY_ID = "SEARCH_BY_ID";
-const SEARCH_BY_NAME = "SEARCH_BY_NAME";
-
-const LOAD_DATA = "LOAD_DATA"
-
-export const loadData = payload => ({
-    type: LOAD_DATA,
-    payload
-});
-
-export const sortById = payload => ({
-    type: SORT_BY_ID,
-    payload
-});
-export const sortByName = payload => ({
-    type: SORT_BY_NAME,
-    payload
-});
-export const sortBySizeTotal = payload => ({
-    type: SORT_BY_SIZE_TOTAL,
-    payload
-});
-
-export const searchById = payload => ({
-    type: SEARCH_BY_ID,
-    payload
-});
-export const searchByName = payload => ({
-    type: SEARCH_BY_NAME,
-    payload
-});
-
 const initialState = {
     audiences: [],
     filteredRecords: [],
@@ -55,6 +19,54 @@ const initialState = {
 const audienceSlice = createSlice({
     name: 'audience',
     initialState: initialState,
+    reducers: {
+        searchById(state, action) {
+            let val = action.payload.value;
+
+            let filtered = state.audiences.filter(aud => {
+                return aud.id === val;
+            });
+            return {
+                ...state,
+                audiences: filtered,
+            };
+        },
+        searchByName(state, action) {
+            let val = action.payload.value.toLowerCase();
+            //if val is cleared reset filter
+            let filteredRecords = state.audiences.filter(aud => {
+                // console.log(aud.name.toLowerCase().includes(val))
+                return aud.name.toLowerCase().includes(val);
+            });
+            state.pagesTotal = Math.ceil(filteredRecords.length / state.pageSize)
+            state.page = 0;
+            //always show 1st page content
+            const displayedRecords = filteredRecords.slice(0, state.pageSize)
+            return {
+                ...state,
+                filteredRecords,
+                displayedRecords
+            };
+        },
+        // sortBySizeTotal(state, action) {
+        //     //sort filteredRecords
+        // },
+        // sortByName(state, action) {
+        //     //sort filteredRecords
+        // },
+        // sortById(state, action) {
+        //     //sort filteredRecords
+        // },
+        loadData(state, action) {
+            const page = action.payload.page
+            console.log("in load data for page: " + page)
+            let stateCopy = Object.assign({}, state)
+            const startIndex = page*state.pageSize
+            //to paginate filtered records, slice state.filtered
+            let displayedRecords = stateCopy.audiences.slice(startIndex, startIndex + state.pageSize)
+            return {...state, displayedRecords, page}
+        }
+    },
     extraReducers: {
         [fetchAudiences.pending]: (state, action) => {
             state.status = "loading";
@@ -72,60 +84,15 @@ const audienceSlice = createSlice({
         [fetchAudiences.rejected]: (state, action) => {
             state.status = "failed";
             state.error = action.payload;
-        },
-        "SEARCH_BY_ID": (state, action) => {
-            let val = action.payload.value;
-
-            let filtered = state.audiences.filter(aud => {
-                return aud.id === val;
-            });
-            return {
-                ...state,
-                audiences: filtered,
-            };
-        },
-        "SEARCH_BY_NAME": (state, action) => {
-            let val = action.payload.value.toLowerCase();
-            //if val is cleared reset filter
-            let filteredRecords = state.audiences.filter(aud => {
-                // console.log(aud.name.toLowerCase().includes(val))
-                return aud.name.toLowerCase().includes(val);
-            });
-            state.pagesTotal = Math.ceil(filteredRecords.length / state.pageSize)
-            state.page = 0;
-            //always show 1st page content
-            const displayedRecords = filteredRecords.slice(0, state.pageSize)
-            return {
-                ...state,
-                filteredRecords,
-                displayedRecords
-            };
-        },
-        "SORT_BY_SIZE_TOTAL": (state, action) => {
-            //sort filteredRecords
-        },
-        "SORT_BY_NAME": (state, action) => {
-            //sort filteredRecords
-        },
-        "SORT_BY_ID": (state, action) => {
-            //sort filteredRecords
-        },
-        "LOAD_DATA": (state, action) => {
-            const page = action.payload.page
-            console.log("in load data for page: " + page)
-            let stateCopy = Object.assign({}, state)
-            const startIndex = page*state.pageSize
-            //to paginate filtered records, slice state.filtered
-            let displayedRecords = stateCopy.audiences.slice(startIndex, startIndex + state.pageSize)
-            return {...state, displayedRecords, page}
         }
-    },
+    }
 })
 export const selectAllAudiences = (state) => state.audience.audiences;
 export const selectDisplayedRecords = (state) => state.audience.displayedRecords;
 export const selectPage = (state) => state.audience.page;
 export const selectPagesTotal = (state) => state.audience.pagesTotal;
 export const selectAudienceStatus = (state) => state.audience.status;
+export const { loadData, searchById, searchByName, sortById, sortByName, sortBySizeTotal } = audienceSlice.actions;
 
 const store = configureStore({
     reducer: combineReducers({
